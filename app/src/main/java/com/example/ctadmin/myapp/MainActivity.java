5a87e41205a53845aad048d20d154f03f99f8692
ctadmin <keshab2610@gmail.com>
2017-02-18 16:10:00 +0530

    com

--- app/src/main/java/com/example/ctadmin/myapp/MainActivity.java
+++ app/src/main/java/com/example/ctadmin/myapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.ctadmin.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        Main2Activity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
